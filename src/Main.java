b4fffd72e9a27442b83c6e29581286ce41380d1d
Jon Bertelsen <jonb@kea.dk>
2018-01-22 22:12:31 +0100

    LocalDateTime small experiments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args)
     {
+
+        LocalDateTime nu = LocalDateTime.now();
+
+        System.out.println("Nu: " + nu);
+
+        LocalDateTime imorgen = LocalDateTime.of(2018,01,23,10,0);
+
+        System.out.println("I morgen: " + imorgen);
+
+        LocalDateTime iovermorgen =  imorgen.plusDays(1);
+
+        System.out.println("I overmorgen: " + iovermorgen);
 
     }
 }
